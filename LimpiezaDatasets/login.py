e1e91948841e817b351164c29c50a85a763e8b7e
isai <73376065+isai2506@users.noreply.github.com>
2024-08-09 09:36:41 -0600

    Update login.py

--- LimpiezaDatasets/login.py
+++ LimpiezaDatasets/login.py
@@ -13,40 +13,57 @@ def validarUsuario(usuario,clave):
     Returns:
         bool: True usuario valido, False usuario invalido
     """    
-    dfusuarios = pd.read_csv('LimpiezaDatasets/usuarios.csv')
+    dfusuarios = pd.read_csv('usuarios.csv')
     if len(dfusuarios[(dfusuarios['usuario']==usuario) & (dfusuarios['clave']==clave)])>0:
         return True
     else:
         return False
 
 def generarMenu(usuario):
+    """Genera el menú dependiendo del usuario
+
+    Args:
+        usuario (str): usuario utilizado para generar el menú
+    """        
     with st.sidebar:
-        dfusuarios = pd.read_csv('LimpiezaDatasets/usuarios.csv')
-        nombre = dfusuarios[dfusuarios['usuario'] == usuario]['nombre'].values[0]
-        st.write(f"Hola **:blue-background[{nombre}]**")
+        # Cargamos la tabla de usuarios
+        dfusuarios = pd.read_csv('usuarios.csv')
+        # Filtramos la tabla de usuarios
+        dfUsuario =dfusuarios[(dfusuarios['usuario']==usuario)]
+        # Cargamos el nombre del usuario
+        nombre= dfUsuario['nombre'].values[0]
+        #Mostramos el nombre del usuario
+        st.write(f"Hola **:blue-background[{nombre}]** ")
+        # Mostramos los enlaces de páginas
         st.page_link("inicio.py", label="Inicio", icon=":material/home:")
         st.subheader("Tableros")
-        st.page_link("pages/pagina1.py", label="Ventas", icon=":material/sell:")
-        st.page_link("pages/pagina2.py", label="Compras", icon=":material/shopping_cart:")
-        st.page_link("pages/pagina3.py", label="Personal", icon=":material/group:")
-        
-        btnSalir = st.button("Salir")
+        st.page_link("pages/pagina1.py", label="KELQ", icon=":material/sell:")
+        st.page_link("pages/pagina2.py", label="AES", icon=":material/shopping_cart:")
+        st.page_link("pages/pagina3.py", label="FTV", icon=":material/group:")    
+        # Botón para cerrar la sesión
+        btnSalir=st.button("Salir")
         if btnSalir:
             st.session_state.clear()
-            st.experimental_js("window.location.reload()")  # Redirige al usuario después de cerrar sesión
+            # Luego de borrar el Session State reiniciamos la app para mostrar la opción de usuario y clave
+            st.rerun()
 
 def generarLogin():
+    """Genera la ventana de login o muestra el menú si el login es valido
+    """    
+    # Validamos si el usuario ya fue ingresado    
     if 'usuario' in st.session_state:
-        generarMenu(st.session_state['usuario'])
-    else:
+        generarMenu(st.session_state['usuario']) # Si ya hay usuario cargamos el menu        
+    else: 
+        # Cargamos el formulario de login       
         with st.form('frmLogin'):
             parUsuario = st.text_input('Usuario')
-            parPassword = st.text_input('Password', type='password')
-            btnLogin = st.form_submit_button('Ingresar', type='primary')
+            parPassword = st.text_input('Password',type='password')
+            btnLogin=st.form_submit_button('Ingresar',type='primary')
             if btnLogin:
-                if validarUsuario(parUsuario, parPassword):
-                    st.session_state['usuario'] = parUsuario
-                    st.experimental_js("window.location.reload()")  # Redirige después del login exitoso
+                if validarUsuario(parUsuario,parPassword):
+                    st.session_state['usuario'] =parUsuario
+                    # Si el usuario es correcto reiniciamos la app para que se cargue el menú
+                    st.rerun()
                 else:
-                    st.error("Usuario o clave inválidos", icon=":material/gpp_maybe:")
-                       
+                    # Si el usuario es invalido, mostramos el mensaje de error
+                    st.error("Usuario o clave inválidos",icon=":material/gpp_maybe:")  
